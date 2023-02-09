9eb60672c6821b7efe87b4c520cfdd747fc2e6bd
sebamawa <sebamawa@hotmail.com>
2023-02-09 12:31:28 -0300

    cust active field in details customer

--- src/pages/CustomerDetailsPage.js
+++ src/pages/CustomerDetailsPage.js
@@ -1,10 +1,15 @@
 import { useParams, useLocation } from "react-router-dom";
 import { CustomerInvoicesPage } from "./CustomerInvoicesPage";
+import { useState } from "react";
 
 function CustomerDetailsPage() {
     const parms = useParams();
     const location = useLocation();
     const { customer } = location.state; 
+
+    const [cust_active, setCustActive] = useState(customer.cust_active);
+
+    console.log(customer);
 
     return (
         <>
@@ -16,8 +21,14 @@ function CustomerDetailsPage() {
                         <div className={"card-body"}>
                             <h5 className={"card-title text-black"}>{customer.cust_fullname}</h5> 
                             <h6 className="text-black">{`Identification: ${customer.cust_identification}`}</h6>
-                            <h6 className="text-black">{`Phone: ${customer.cust_phone}`}</h6> 
-                        </div>            
+                            <h6 className="text-black">{`Phone: ${customer.cust_phone}`}</h6>
+                            <div class="form-check">
+                                <input className="form-check-input" type="checkbox" value={cust_active} id="cust_active" checked={cust_active}/>
+                                <label className="form-check-label text-black" for="cust_active">
+                                    <h6>Is active?</h6>
+                                </label>
+                            </div>            
+                        </div>
                     </div>
                 </div>
                 <div className="col-7">
